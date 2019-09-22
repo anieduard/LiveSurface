8574f8150e7974ae96ca669fa43d5139a7eae042
Ani Eduard <eduard@halcyonmobile.com>
2019-09-22 12:22:56 +0300

    Initial Commit

---
+++ LiveSurface/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LiveSurface
+//
+//  Created by Ani Eduard on 22/09/2019.
+//  Copyright © 2019 Ani Eduard. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
